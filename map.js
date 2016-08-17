3ba1dd1fac3697a3303c0dbcd50317808d31e965
Tim Channell <tim.channell@gmail.com>
2016-08-17 11:58:45 -0400

    fix raw

--- map.js
+++ map.js
@@ -4,17 +4,19 @@ const Changelog = require('./changelog');
 const turf = require('turf');
 
 // Turn BBOX into point (for overview)
-function generalizeAsPoint(ft) {
-    return turf.centroid(ft);
+function generalizeAsPoint(ft, tile) {
+    return turf.centroid(ft.toGeoJSON(tile[0], tile[1], tile[2]));
 }
 
 module.exports = function(tileLayers, tile, write, done) {
   const changelog = new Changelog(tile);
-  tileLayers.osm.osm.features.forEach(ft => changelog.track(ft));
+  for (var i = 0; i < tileLayers.osm.osm.length; i++) {
+    changelog.track(tileLayers.osm.osm.feature(i));
+  }
 
   const feature = changelog.toGeoJSON();
   if(global.mapOptions.usePoint) {
-    const pointFeature = generalizeAsPoint(feature);
+    const pointFeature = generalizeAsPoint(feature, tile);
     pointFeature.properties = feature.properties;
     done(null, pointFeature);
   } else {
